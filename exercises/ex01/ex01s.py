f58dfaf3f65e1624de8f61248ec0d115709a2f58
Shilpa M S <msshilpa1895@gmail.com>
2024-11-08 11:23:46 +0100

    E1Q2 färdigt

--- exercises/ex01/ex01s.py
+++ exercises/ex01/ex01s.py
@@ -1,4 +1,4 @@
-# Q1
+# Q1 Pandemic rules
 def norway_pandemic():
     region = input("Hvor er du bosatt? ")
     if region[0] == "V" or region[1] == "V":
@@ -17,4 +17,31 @@ def norway_pandemic():
             else:
                 print("Velkommen til Norge, men du må teste deg och sitte i karantene.")
 
-norway_pandemic()
+# norway_pandemic()
+
+
+# Q2 Ordering drinks
+def price(drinks):
+    prices = [("kaffe", 30), ("öl", 50), ("kola", 25)]
+    [qty, name] = drinks.split()
+    for (item, rate) in prices:
+        if item == name:
+            return rate*int(qty)
+    return 0
+
+def get_order():
+    order = input("Vad vill ni dricka? ")
+    total = 0
+    while order != "Det är bra så":
+        p = price(order)
+        if p == 0:
+            print("Finns tyvärr inte ")
+        total += p
+        order = input("Något mer? ")
+    print("Det blir", total, "kronor")
+
+#get_order()
+
+# Q3 Scrambled text
+
+
